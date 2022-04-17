77b0bc0ffe37b113c935be27f64f86e06cf5323f
Isaak Morales <ice.isaak97@gmail.com>
2022-04-17 16:18:40 -0400

    switched into making switch statements instead

--- questions.js
+++ questions.js
@@ -3,163 +3,70 @@ const inquirer = require("inquirer");
 const mysql2 = require("mysql2")
 const express = ("express")
 
-function () {
+function questionPrompts() {
     inquirer
-        .prompt([{
+        .prompt({
             type: "list",
             name: "employeeTitle",
             message: "What would you like to do?",
             choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
-        }
-
-        ]).then((answer) => {
-            if (answer.employeeTitle === "Engineer") {
-                //* Call engineer function
-                function engineerCard() {
-                    inquirer
-                        .prompt([
-                            {
-                                type: "input",
-                                name: "name",
-                                message: "What is your engineer's name?",
-                            },
-                            {
-                                type: "input",
-                                name: "id",
-                                message: "What is your engineer's id?",
-                            },
-                            {
-                                type: "input",
-                                name: "email",
-                                message: "What is your engineer's email?",
-                            },
-                            {
-                                type: "input",
-                                name: "github",
-                                message: "Engineer's GitHub Profile:",
-                            }
-                        ]).then((answer) => {
-                            //obj creation
-                            console.log("this is the data for engineer", Engineer);
-                            let engineerDiv = `          <div class="card" style="width: 18rem;">
-                            <div class="card-header">
-                              Engineer
-                            </div>
-                            <ul class="list-group list-group-flush">
-                              <li class="list-group-item">${answer.name}</li>
-                              <li class="list-group-item">${answer.id}</li>
-                              <li class="list-group-item"><a href="${answer.email}">${answer.email}</a></li>
-                              <li class="list-group-item"><a href="www.github.com/${answer.github}">www.github.com/${answer.github}</a></li>
-                            </ul>
-                            </div>`
-                            console.log(engineerDiv);
-                            teamCreation.push(engineerDiv);
-                            teamBuilder();
-                            console.log(teamCreation);
-                            console.log(answer);
-                        })
+        }) 
+            .then((answer) => {
+                switch (expression) {
+                    case "View All Employees":
+                      //Statements executed when the
+                      //result of expression matches value1
+                      [break;]
+                    case "Add Employee":
+                      //Statements executed when the
+                      //result of expression matches value2
+                      [break;]
+                    
+                    case "Update Employee Role":
+                      //Statements executed when the
+                      //result of expression matches valueN
+                      [break;]
+                    case "View All Roles"
+                      //Statements executed when none of
+                      //the values match the value of the expression
+                      [break;]
+                    case "Add Role":
+                      //Statements executed when the
+                      //result of expression matches value2
+                      [break;]
+                    
+                    case "View All Departments":
+                      //Statements executed when the
+                      //result of expression matches valueN
+                      [break;]
+                    case "Add Department"
+                      //Statements executed when none of
+                      //the values match the value of the expression
+                      [break;]
                 }
-                engineerCard();
-            } else if (answer.employeeTitle === "Intern") {
-                //*Call intern function
-                function internCard() {
-                    inquirer
-                        .prompt([
-                            {
-                                type: "input",
-                                name: "name",
-                                message: "What is your intern's name?",
-                            },
-                            {
-                                type: "input",
-                                name: "id",
-                                message: "What is your intern's id?",
-                            },
-                            {
-                                type: "input",
-                                name: "email",
-                                message: "What is your intern's email?",
-                            },
-                            {
-                                type: "input",
-                                name: "school",
-                                message: "What is your intern's school name?:",
-                            }
-                        ]).then((answer) => {
-                            //obj creation
+            })}
+                
+        // switch (expression) {
+        //     case value1:
+        //       Statements executed when the
+        //       result of expression matches value1
+        //       [break;]
+        //     case value2:
+        //       Statements executed when the
+        //       result of expression matches value2
+        //       [break;]
+        //     ...
+        //     case valueN:
+        //       Statements executed when the
+        //       result of expression matches valueN
+        //       [break;]
+        //     [default:
+        //       Statements executed when none of
+        //       the values match the value of the expression
+        //       [break;]]
+        //   }
 
-                            console.log(Intern);
-                            let internDiv = `          <div class="card" style="width: 18rem;">
-                            <div class="card-header">
-                              Intern
-                            </div>
-                            <ul class="list-group list-group-flush">
-                              <li class="list-group-item">${answer.name}</li>
-                              <li class="list-group-item">${answer.id}</li>
-                              <li class="list-group-item"><a href="${answer.email}">${answer.email}</a></li>
-                              <li class="list-group-item">${answer.school}</li>
-                            </ul>
-                            </div>`
-                            console.log(internDiv);
-                            teamCreation.push(internDiv);
-                            teamBuilder();
-                        })
-                }
-                internCard();
-            } else if (answer.employeeTitle === 'Manager') {
-                function managerCard() {
-                    inquirer
-                        .prompt([
-                            {
-                                type: "input",
-                                name: "name",
-                                message: "What is your manager's name?",
-                            },
-                            {
-                                type: "input",
-                                name: "id",
-                                message: "What is your manager's id?",
-                            },
-                            {
-                                type: "input",
-                                name: "email",
-                                message: "What is your manager's email?",
-                            },
-                            {
-                                type: "input",
-                                name: "officeNumber",
-                                message: "Manager's Office Number:",
-                            }])
-                        .then((answer) => {
-                            const manager = new Manager(
-                                answer.name, answer.id, answer.email, answer.officeNumber
-                            )
-                            console.log(manager);
-                            let managerDiv = `          <div class="card" style="width: 18rem;">
-                            <div class="card-header">
-                              Manager
-                            </div>
-                            <ul class="list-group list-group-flush">
-                              <li class="list-group-item">${answer.name}</li>
-                              <li class="list-group-item">${answer.id}</li>
-                              <li class="list-group-item"><a href="${answer.email}">${answer.email}</a></li>
-                              <li class="list-group-item">${answer.officeNumber}</li>
-                            </ul>
-                            </div>`
-                            console.log(managerDiv);
-                            teamCreation.push(managerDiv);
-                            teamBuilder();
-                        });
-                }
-                managerCard();
-            } else if (answer.employeeTitle === 'Done') {
-                console.log("-----------", teamCreation);
-                //*Call the done and write everything to html file
-                fs.writeFile("cards.html", markdown (teamCreation), (err) => err ? console.log(err) : console.log("Success"))
-            }
-        })
-}
 
-teamBuilder();
+questionPrompts();
 
 module.exports = teamBuilder;
