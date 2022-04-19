8d23d04cded89a3a8453f4220b6ba1cd851baf48
Isaak Morales <ice.isaak97@gmail.com>
2022-04-18 21:31:47 -0400

    Got all the add functions seet up

--- questions.js
+++ questions.js
@@ -2,58 +2,90 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const mysql2 = require("mysql2")
 const express = ("express")
-const connect = require('../db/connection')
+const connect = require('./db/connection')
 
 function questionPrompts() {
-    inquirer
-        .prompt({
-            type: "list",
-            name: "choice",
-            message: "What would you like to do?",
-            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
-        }) 
-            .then((data) => {
-                switch (data[choice]) {
-                    case "View All Employees":
-                      viewAllEmployees();
-                      break;
-                    case "Add Employee":
-                      addEmployee();
-                      break;
-                    case "Update Employee Role":
-                      updateEmployeeRole();
-                      break;
-                    case "View All Roles":
-                      viewAllRoles();
-                      break;
-                    case "Add Role":
-                      addRole();
-                      break;
-                    case "View All Departments":
-                     viewAllDepartments();
-                      break;
-                    case "Add Department":
-                      addDepartment();
-                      break;
-                }
-            })}
-                
-  
-            const addDepartment = () => {
-              inquirer
-                  .prompt({
-                      type: 'text',
-                      name: 'departmentName',
-                      message: 'Enter the Department name you would like to add:'
-                  }).then ((answer) => {
-                      connect.query ('INSERT INTO department (dept_name) VALUES (?)', answer.departmentName, (err, res) => {
-                          if (err) throw (err)
-                          console.log(answer.departmentName);
-                          questionPrompts();
-                      })
-                  })
-          }
-          
+  inquirer
+    .prompt({
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
+    })
+    .then((data) => {
+      switch (data) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    })
+}
+
+
+const addDepartment = () => {
+  inquirer
+    .prompt({
+      type: 'text',
+      name: 'departmentName',
+      message: 'Enter the Department name you would like to add:'
+    }).then((answer) => {
+      connect.query('INSERT INTO department (dept_name) VALUES (?)', answer.departmentName, (err, res) => {
+        if (err) throw (err)
+        console.log(answer.departmentName);
+        questionPrompts();
+      })
+    })
+}
+
+
+const addRole = () => {
+  inquirer
+    .prompt({
+      type: 'text',
+      name: 'roleName',
+      message: 'Enter the Role name you would like to add:'
+    }).then((answer) => {
+      connect.query('INSERT INTO roles (title, salary, department_id) VALUES (?)', answer.roleName, (err, res) => {
+        if (err) throw (err)
+        console.log(answer.roleName);
+        questionPrompts();
+      })
+    })
+}
+
+
+const addEmployee = () => {
+  inquirer
+    .prompt({
+      type: 'text',
+      name: 'employeeName',
+      message: 'Enter the Employee name you would like to add:'
+    }).then((answer) => {
+      connect.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)', answer.employeeName, (err, res) => {
+        if (err) throw (err)
+        console.log(answer.employeeName);
+        questionPrompts();
+      })
+    })
+}
 
 questionPrompts();
 
